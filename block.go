2d6c03c301372b55435289b298d9903ab43367a0
Mooje Kong <mooje.kong@weather.com>
2021-07-17 11:47:34 +0900

    add util

--- block.go
+++ block.go
@@ -8,16 +8,21 @@ import (
 )
 
 type Block struct {
-	Timestamp  			int64
-	Data 				[]byte
-	PrevBlockHash		[]byte
-	Hash				[]byte
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
 
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
 
 	return block
 }
